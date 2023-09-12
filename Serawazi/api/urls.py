3c05829a0f2a458718e9dab332a3eadccbaf8236
midevamarrion <midevahmarrion@gmail.com>
2023-09-12 13:06:35 +0300

    removed extra files

--- Serawazi/api/urls.py
+++ Serawazi/api/urls.py
@@ -1,8 +1,11 @@
-# urls.py
+# Import your views and necessary modules
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('collections/', views.ScenarioCollectionListCreateView.as_view(), name='collection-list-create'),
-    path('collections/<int:pk>/', views.ScenarioCollectionRetrieveUpdateDestroyView.as_view(), name='collection-detail'),
+    # URL pattern for the list and create view
+    path('collections/', views.ScenarioCollectionListCreateView.as_view(), name='scenario-collection-list-create'),
+
+    # URL pattern for the retrieve, update, and delete view
+    path('collections/<int:id>/', views.ScenarioCollectionRetrieveUpdateDestroyView.as_view(), name='scenario-collection-retrieve-update-destroy'),
 ]
--- Serawazi/api/views.py
+++ Serawazi/api/views.py
@@ -8,17 +8,31 @@ class ScenarioCollectionListCreateView(generics.ListCreateAPIView):
     serializer_class = ScenarioCollectionSerializer
 
     def create(self, request, *args, **kwargs):
+        return self.create_scenario(request)
+
+    def create_scenario(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def list(self, request, *args, **kwargs):
+        return self.list_scenarios(request)
+
+    def list_scenarios(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 class ScenarioCollectionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ScenarioCollection.objects.all()
     serializer_class = ScenarioCollectionSerializer
 
     def update(self, request, *args, **kwargs):
+        return self.update_scenario(request, *args, **kwargs)
+
+    def update_scenario(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
@@ -27,7 +41,18 @@ class ScenarioCollectionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroy
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def retrieve(self, request, *args, **kwargs):
+        return self.retrieve_scenario(request, *args, **kwargs)
+
+    def retrieve_scenario(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
     def destroy(self, request, *args, **kwargs):
+        return self.destroy_scenario(request, *args, **kwargs)
+
+    def destroy_scenario(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)

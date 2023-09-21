1d787652057a4e0fa7f8151c3450785350a58f3a
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-09-22 01:21:48 +0300

    Created static files, runtime.txt and Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SeraWazi.wsgi --log-file -
--- Serawazi/settings.py
+++ Serawazi/settings.py
@@ -13,7 +13,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -167,3 +167,12 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+import django_heroku
+django_heroku.settings(locals())
+
+
+
+
+
+

a3d059a0c1d490f336f2cd24248838a27072d307
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-09-22 08:56:25 +0300

    added env1 on git ignore

--- Serawazi/settings.py
+++ Serawazi/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
 MEDIA_URL='/images/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'images')
 
-
+DISABLE_COLLECTSTATIC = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

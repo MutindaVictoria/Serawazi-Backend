a323b4a00a0fdd8ddd3996d9e9ed7595a3e5148f
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-09-22 09:41:55 +0300

    added env1 on git ignore

--- Serawazi/settings.py
+++ Serawazi/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
